9ddf213ac5236214d87d06fecfb621e37f69f481
daniel <daniel.tebart@googlemail.com>
2022-12-18 21:49:27 +0100

    Introduced column to show minimum odds for positive ev.

--- src/custom_components/HorseRow.js
+++ src/custom_components/HorseRow.js
@@ -5,8 +5,9 @@ export default class HorseRow extends React.Component {
   render() {
     return (
       <tr>
-        <td>{this.props.id}</td>
-        <td>{this.props.win_prob}</td>
+          <td>{this.props.id}</td>
+          <td>{this.props.win_prob}</td>
+          <td>{this.props.value_thresh}</td>
       </tr>
     );
   }
--- src/custom_components/ValueTable.js
+++ src/custom_components/ValueTable.js
@@ -6,15 +6,15 @@ export default class ValueTable extends React.Component {
         super(props);
         this.state = {
             horses: [
-                {id: 1, win_prob: 0.25},
-                {id: 2, win_prob: 0.5},
-                {id: 3, win_prob: 0.25},
+                {id: 1, win_prob: 0.0, value_thresh: 0.0},
+                {id: 2, win_prob: 0.0, value_thresh: 0.0},
+                {id: 3, win_prob: 0.0, value_thresh: 0.0},
             ]
         };
     }
 
     componentDidMount() {
-        this.timerID = setInterval(() => this.tick(),2000);
+        this.timerID = setInterval(() => this.tick(),5000);
     }
 
     tick() {
@@ -25,11 +25,14 @@ export default class ValueTable extends React.Component {
         const random_win_probs = random_scores.map(random_score => {
             return ((random_score / scores_sum) * 100).toFixed(2)
         })
+        const value_thresh = random_win_probs.map(win_prob => {
+            return (100 / win_prob).toFixed(2)
+        })
 
         const n = this.state.horses.length;
         let new_horses = []
         for (let i = 0; i < n; ++i) {
-            new_horses[i] = {id: i+1, win_prob: random_win_probs[i]};
+            new_horses[i] = {id: i+1, win_prob: random_win_probs[i], value_thresh: value_thresh[i]};
         }
 
         this.setState({
@@ -43,11 +46,12 @@ export default class ValueTable extends React.Component {
                 <tr>
                     <th>Horse ID</th>
                     <th>Win probability</th>
+                    <th>Min odds for value</th>
                 </tr>
               <tbody>
               {
                 this.state.horses.map((horse) =>
-                <HorseRow id={horse.id} win_prob={horse.win_prob}/>
+                <HorseRow id={horse.id} win_prob={horse.win_prob} value_thresh={horse.value_thresh}/>
                 )}
               </tbody>
           </table>
